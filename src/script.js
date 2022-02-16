c92650f9dfb4fe57a610c1cfefb91675957513f2
Steffani <sartinisteffani@gmail.com>
2022-02-16 19:06:14 +0100

    Header and Footer setup

--- src/script.js
+++ src/script.js
@@ -7,10 +7,15 @@
 
 
 
-/*Steffani code*/
+/*
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target.elements.desc.value, e.target.elements.status.value);
+  });
 
 
-
-
+Steffani code*/
 
 /*Nett code*/
